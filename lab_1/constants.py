de56f59b32543d9855e3ba3999badd4dc09c9abb
Leshechka123 <alexey_2004@inbox.ru>
2024-05-03 14:05:12 +0400

    try 2nd task

--- lab_1/constants.py
+++ lab_1/constants.py
@@ -1,3 +1,4 @@
 dict_file_path = "C:\\Users\\User\\Desktop\\oib\\isb\\lab_1\\task 1\\key1.json"
 text_file_path = "C:\\Users\\User\\Desktop\\oib\\isb\\lab_1\\task 1\\text.txt"
 encrypted_text_file_path = "C:\\Users\\User\\Desktop\\oib\\isb\\lab_1\\task 1\\encrypted_text.txt"
+cipher = "C:\\Users\\User\\Desktop\\oib\\isb\\lab_1\\task 2\\cipher.txt"
--- lab_1/main1.py
+++ lab_1/main1.py
@@ -1,35 +1,50 @@
+import os
 import json
+from collections import Counter
+from constants import *
+from encrypt_text_with_dict import *
 
-# Функция для шифрования текста с использованием словаря
-def encrypt_text_with_dict(text, dictionary):
-    encrypted_text = ""
-    for char in text:
-        if char in dictionary:
-            encrypted_text += dictionary[char]
-        else:
-            encrypted_text += char
-    return encrypted_text
-
-# Загрузка словаря из JSON файла
 def load_dict_from_json(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
 
-# Получение пути к JSON файлу с словарем и к исходному текстовому файлу
-dict_file_path = "C:\\Users\\User\\Desktop\\oib\\isb\\lab_1\\task 1\\key1.json"
-text_file_path = "C:\\Users\\User\\Desktop\\oib\\isb\\lab_1\\task 1\\text.txt"
-encrypted_text_file_path = "C:\\Users\\User\\Desktop\\oib\\isb\\lab_1\\task 1\\encrypted_tex.txt"
+def frequency_analysis(input_file_path: str, output_file_path: str) -> None:
+    with open(input_file_path, 'r', encoding='utf-8') as file:
+        text = file.read().replace('\n', '')  # Убираем символы переноса строки из текста
+    print(text)
 
-# Загрузка словаря
-dictionary = load_dict_from_json(dict_file_path)
+    total_chars = len(text)
 
-# Чтение исходного текста
-with open(text_file_path, 'r', encoding='utf-8') as file:
-    text = file.read()
+    frequencies = {}
+    for char in text:
+        if char != '\n':  # Игнорируем символы переноса строки
+            if char in frequencies:
+                frequencies[char] += 1
+            else:
+                frequencies[char] = 1
 
-# Шифрование текста с использованием словаря
-encrypted_text = encrypt_text_with_dict(text, dictionary)
+    probabilities = {char: count / total_chars for char, count in frequencies.items()}
 
-with open(encrypted_text_file_path, 'w', encoding='utf-8') as file:
-    file.write(encrypted_text)
+    with open(output_file_path, 'w', encoding='utf-8') as output_file:
+        json.dump(probabilities, output_file, ensure_ascii=False, indent=4)
+
+
+
+if __name__ == "main":
+    dictionary = load_dict_from_json(dict_file_path)
+
+    with open(text_file_path, 'r', encoding='utf-8') as file:
+        text = file.read()
+
+    encrypted_text = encrypt_text_with_dict(text, dictionary)
+
+    with open(encrypted_text_file_path, 'w', encoding='utf-8') as file:
+        file.write(encrypted_text)
+
+    text_path = "C:\\Users\\User\\Desktop\\oib\\isb\\lab_1\\task 2\\cipher.txt"
+    output_path = "C:\\Users\\User\\Desktop\\oib\\isb\\lab_1\\task 2\\frequency_analysis.json"
+    with open(cipher, 'r', encoding='utf-8') as file:
+        text = file.read().replace('\n', '')  # Убираем символы переноса строки из текста
+    print(text)
+    frequency_analysis(text_path, output_path)
